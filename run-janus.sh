69c80d18c34dba60661f68d00c24f7ca3acc4c46
Rammohan <ram@etherlabs.io>
2019-03-15 08:44:42 +0530

    increase log level to 5

--- run-janus.sh
+++ run-janus.sh
@@ -2,6 +2,7 @@
 
 sed -i 's/#stun_server = "stun.voip.eutelia.it"/stun_server = "stun.l.google.com"/g' /usr/local/etc/janus.jcfg
 sed -i 's/#stun_port = 3478/stun_port = 19302/g' /usr/local/etc/janus/janus.jcfg
+sed -i 's/debug_level = 4/debug_level = 5/g' /usr/local/etc/janus/janus.jcfg
 sed -i 's/#debug_timestamps = true/debug_timestamps = true/g' /usr/local/etc/janus/janus.jcfg
 
 sed -i 's/#nat_1_1_mapping = "1.2.3.4"/nat_1_1_mapping = "'${PUBLIC_IP}'"/g' /usr/local/etc/janus/janus.jcfg
